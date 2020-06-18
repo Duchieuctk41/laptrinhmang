71b3c6dc44c62f0c14a1e40bb9495aa7f9e821ac
HieuHoc <duchieu.ctk41@gmail.com>
2020-06-18 10:42:31 +0700

    mas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const bodyParser = require("body-parser");
 const direction = require('./routes/direction.router');
 const server = require('http').Server(app);
